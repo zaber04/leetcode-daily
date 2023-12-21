8bb2fe7b97032ca4e3a9c5788825740a773b6acc
Kazi Zaber <zaber.mahbub@gmail.com>
2023-12-21 21:35:24 +0600

    Time: 0 ms (100.00%), Space: 6.6 MB (98.75%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,30 +3,19 @@ public:
     string mergeAlternately(string word1, string word2) {
         int len1 = word1.length();
         int len2 = word2.length();
+        string out = "";
         
-        string s = "";
-        int smaller, larger;
-        string word;
-        if (len1 < len2) {
-            smaller = len1;
-            larger = len2;
-            word = word2;
-        } else {
-            smaller = len2;
-            larger = len1;
-            word = word1;
+        for (int i = 0, limit = max(len1, len2); i < limit; i++) {
+            if (i < len1) {
+                out.push_back(word1[i]);
+            }
+
+            if (i < len2) {
+                out.push_back(word2[i]);
+            }
         }
         
-        for (int i=0; i<smaller;i++) {            
-            s += word1[i];
-            s += word2[i];
-        }
-        
-        for (int i=smaller; i<larger;i++) {
-            s += word[i];
-        }
-        
-        return s;
+        return out;
         
     }
 };
