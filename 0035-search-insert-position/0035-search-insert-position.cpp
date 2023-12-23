58fadee01c49b75ca7a49318f44b6c67e3e844bf
Kazi Zaber <zaber.mahbub@gmail.com>
2023-12-24 02:10:43 +0600

    Time: 0 ms (100.00%), Space: 10.1 MB (34.28%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return low;
+    }
+};
