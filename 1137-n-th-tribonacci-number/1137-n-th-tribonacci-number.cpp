22cc8d709dbe564b98d5921fdabb74f631c3f59d
Kazi Zaber <zaber.mahbub@gmail.com>
2024-04-24 15:40:41 +0600

    Time: 0 ms (100%), Space: 7.1 MB (81.41%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,38 +1,21 @@
-#define MAX_NUM 37
-
 class Solution {
-    vector<int> memo;
-
-    int calcTrib(int n) {
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-
-        int t3 = calcTrib(n-3);
-        int t2 = calcTrib(n-2);
-        int t1 = calcTrib(n-1);
-
-        memo[n] = t3 + t2 + t1;
-
-        return memo[n];
-    }
 public:
-    Solution() {
-        memo.push_back(0); // 0
-        memo.push_back(1); // 1
-        memo.push_back(1); // 2
-        memo.push_back(2); // 3
-        memo.push_back(4); // 4
-        memo.push_back(7); // 5
-
-        memo.resize(MAX_NUM + 1, -1);
-    }
-
     int tribonacci(int n) {
-        if (n <= 5) {
-            return memo[n];
+        int a = 0, b = 1, c = 1, t = 0;
+
+        if (n == 0) {
+            return 0;
+        } else if (n < 3) {
+            return 1;
         }
-        
-        return calcTrib(n);
+
+        for (int i = 3; i <= n; i++) {
+            t = a + b + c;
+            a = b;
+            b = c;
+            c = t;
+        }
+
+        return t;
     }
 };
