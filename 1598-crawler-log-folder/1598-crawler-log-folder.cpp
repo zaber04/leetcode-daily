4919fac927b2cd42b98f93cb88d1bd81731c604f
Kazi Zaber <zaber.mahbub@gmail.com>
2024-07-11 01:07:30 +0600

    Time: 8 ms (24.07%), Space: 14.1 MB (37.81%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int level = 0;
+
+        for (auto& dir : logs) {
+            if (dir == "../") {
+                level -= (level > 0);
+            } else if (dir != "./") {
+                level ++;
+            }
+        }
+
+        return level;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
