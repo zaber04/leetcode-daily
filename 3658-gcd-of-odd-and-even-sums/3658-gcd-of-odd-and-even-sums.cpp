5abd8b52b395061b7b84c398e0fa2fc64fbe71e1
Kazi Zaber <zaber.mahbub@gmail.com>
2026-07-15 18:44:03 +0600

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int oddSum(int n) {
+        return n * n;
+    }
+
+    int evenSum(int n) {
+        return n * (n+1);
+    }
+
+    int GCD(int x, int y) {
+        return 0;
+    }
+public:
+    int gcdOfOddEvenSums(int n) {
+        // return GCD(oddSum(n), evenSum(n));
+        return n;
+    }
+};
